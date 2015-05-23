e207147c6734dffa55c8217ef74e0a262c98ae01
Sebastian Porto <sebasporto@gmail.com>
2015-05-23 14:16:24 +1000

    Lint

--- server/services/verifyCode.js
+++ server/services/verifyCode.js
@@ -3,43 +3,39 @@ var expect  = chai.expect;
 
 module.exports = function(code) {
 
-    // try and chai plugin
-    console.log("within verifyCode")
+  // try and chai plugin
+  console.log('within verifyCode')
+  console.log(code);
+
+  // Get result from a sandboxed environment
+  var result = {
+    bearTo: 90,
+    move: 1,
+    aimTo: 90,
+    useWeapon: 'saw'
+  }
+
+  try {
+    // Check bearTo
+    expect([undefined, 0, 90, 180, 270]).to.include(result.bearTo);
+
+    // Check move
+    expect([undefined, -1, 0, 1]).to.include(result.move);
+
+    // Check aimTo
+    expect([undefined, 0, 90, 180, 270]).to.include(result.aimTo);
+
+    // Check weapons
+    expect([undefined, 'flame thrower', 'flipper', 'saw', 'laser']).to.include(result.useWeapon);
+
+    // Good user lets continue
+    console.log('Good user');
     console.log(code);
+  } catch (e) {
+    console.log('Bad user!');
+    console.log(e);
+  }
 
-    // Get result from a sandboxed environment
-    var result = {
-        bearTo: 90, 
-        move: 1,
-        aimTo: 90,
-        useWeapon: "saw"
-    }
-
-    try{
-        // Check bearTo
-        expect([undefined,0,90,180,270]).to.include(result['bearTo']);
-
-        // Check move
-        expect([undefined,-1,0,1]).to.include(result['move']);
-
-        // Check aimTo
-        expect([undefined,0,90,180,270]).to.include(result['aimTo']);
-
-        // Check weapons
-        expect([undefined, "flame thrower", "flipper", "saw", "laser"]).to.include(result['useWeapon']);
-
-        // Good user lets continue
-        console.log("Good user");
-        console.log(code);
-    } catch(e){
-        console.log("Bad user!");
-        console.log(e);
-    }
-
-    
-
-
-
-    //console.log(code['bearTo']);
-    // return something good, bad
+  //console.log(code['bearTo']);
+  // return something good, bad
 }
