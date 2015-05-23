3afd2d404b38fca54d2d99722af1052bd622e7d2
Michael Pope <map7@corsairsolutions.com.au>
2015-05-23 14:09:25 +1000

    Testing shit! Making sure the result from the users code is suitable

--- server/services/verifyCode.js
+++ server/services/verifyCode.js
@@ -8,16 +8,35 @@ module.exports = function(code) {
     console.log(code);
 
     // Get result from a sandboxed environment
-    var result = {}
-
-    // Check bearing
-    console.log(result['bearTo']);
-    try{
-        expect([undefined,0,90,180,270]).to.include(result['bearTo']);
-    } catch(e){
-        console.log("Bad user!")
-        console.log(e)
+    var result = {
+        bearTo: 90, 
+        move: 1,
+        aimTo: 90,
+        useWeapon: "saw"
     }
+
+    try{
+        // Check bearTo
+        expect([undefined,0,90,180,270]).to.include(result['bearTo']);
+
+        // Check move
+        expect([undefined,-1,0,1]).to.include(result['move']);
+
+        // Check aimTo
+        expect([undefined,0,90,180,270]).to.include(result['aimTo']);
+
+        // Check weapons
+        expect([undefined, "flame thrower", "flipper", "saw", "laser"]).to.include(result['useWeapon']);
+
+        // Good user lets continue
+        console.log("Good user");
+        console.log(code);
+    } catch(e){
+        console.log("Bad user!");
+        console.log(e);
+    }
+
+    
 
 
 
