8e8ccd423fa745629c72fba7a00619351ad04b87
Michael Pope <map7@corsairsolutions.com.au>
2015-05-23 22:42:41 +1000

    I have the robot but he is not moving :(

--- client/Game.jsx
+++ client/Game.jsx
@@ -12,9 +12,10 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {game: [{name: "Sonny", x: 50, y: 80}, {name: "Terminator", x: 90, y: 200}]}
+
     props.socket.on('server:loop', (state) => {
-      // console.log(state);
-      
+      console.log(state.robots[0]);
+      this.setState({robot: state.robots[0]})
     });
   }
 
@@ -62,9 +63,7 @@ class Game extends React.Component {
       <div>
       <Surface width={surfaceWidth} height={surfaceHeight} left={0} top={0}>
       <Image style={this.getImageStyle(game[0])} src="imgs/robot1.png" />
-      <Image style={this.getImageStyle(game[1])} src="imgs/robot2.png" />
       <Text style={this.getTextStyle(game[0])}>{this.getRobotName(game[0])}</Text>
-      <Text style={this.getTextStyle(game[1])}>{this.getRobotName(game[1])}</Text>
       </Surface>
       </div>
     );
