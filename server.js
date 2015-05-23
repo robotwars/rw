c3e425144b10805c3265c427bc3a93ac694da296
Sebastian Porto <sebasporto@gmail.com>
2015-05-23 10:38:07 +1000

    Fix

--- server.js
+++ server.js
@@ -71,8 +71,10 @@ var io = require('socket.io')(server);
 io.use(function(socket, next) {
   sessionMiddleware(socket.request, socket.request.res, next);
 });
+
 var config = {
   io:            io
 }
+
 handleSockets(config);
-handleGameLoop(io);
+handleGameLoop(config);
