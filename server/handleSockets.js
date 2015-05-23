6d48e5423c101cbf1b3de6d7a0e7b51db54a4e4f
Sebastian Porto <sebasporto@gmail.com>
2015-05-23 11:08:32 +1000

    Create robot on user connect

--- server/handleSockets.js
+++ server/handleSockets.js
@@ -1,23 +1,29 @@
-// var saveUserInfo = require('.')
+var createRobot  = require('./services/createRobot');
+var saveUserInfo = require('./services/saveUserInfo');
 
 module.exports = function(config) {
   var io             = config.io;
+  var dbConfig       = config.dbConfig;
 
   // user connected
   io.on('connection', function(socket) {
 
     var sockerId = socket.id;
+    var userId = socket.request.session.id;
 
-    console.log('user connected, sockerId', sockerId);
-    console.log('sessionId', socket.request.session.id);
+    // console.log('user connected, sockerId', sockerId);
+    // console.log('sessionId', userId);
 
+    // Create the robot
+    createRobot(dbConfig, userId);
 
     // Send a session token to the user
     // create a robot for this user
     // socket.emit('news', { hello: 'world' });
 
     socket.on('user:info:updated', function(data) {
-      console.log(data.info, socket.id);
+      // console.log(data.info, socket.id);
+      saveUserInfo(userId, data);
     })
 
     socket.on('user:code:updated', function(data) {
