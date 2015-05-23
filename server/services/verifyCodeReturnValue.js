cb350cc456c1767a11573aa17ec8a5e9320652be
Sebastian Porto <sebasporto@gmail.com>
2015-05-23 16:29:20 +1000

    Back to VM2 again

--- server/services/verifyCodeReturnValue.js
+++ server/services/verifyCodeReturnValue.js
@@ -4,28 +4,30 @@ var bluebird = require('bluebird');
 var Joi      = require('joi');
 
 // var valid
-//
+
+// Example expected result
+// {
+//   bearTo: 90,
+//   move: 1,
+//   aimTo: 90,
+//   useWeapon: 'saw'
+// }
+
 var schema = Joi.object().keys({
-  bearTo: Joi.any().valid(undefined, 0, 90, 180, 270),
-  move:   Joi.any().valid(undefined, -1, 0, 1),
-  aimTo:  Joi.any().valid(undefined, 0, 90, 180, 270),
-  useWeapon: Joi.any().valid('flame', 'flipper', 'saw', 'laser')
+  bearTo:        Joi.any().valid(undefined, 0, 90, 180, 270),
+  move:          Joi.any().valid(undefined, -1, 0, 1),
+  aimTo:         Joi.any().valid(undefined, 0, 90, 180, 270),
+  useWeapon:     Joi.any().valid('flame', 'flipper', 'saw', 'laser')
 });
 
 module.exports = function(result) {
 
   // try and chai plugin
   console.log('verifyCodeReturnValue', result);
-
-  // Example expected result
-  // {
-  //   bearTo: 90,
-  //   move: 1,
-  //   aimTo: 90,
-  //   useWeapon: 'saw'
-  // }
+  console.log(_.isString(result))
 
   return new bluebird.Promise(function(resolve, reject) {
+
     return Joi.validate(result, schema, function(err, value) {
       // console.log(err)
       // console.log(value)
