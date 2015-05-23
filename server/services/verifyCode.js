9341f36e6d255fb7b6847a6848001e95e4ea2042
Michael Pope <map7@corsairsolutions.com.au>
2015-05-23 14:00:55 +1000

    Check that the direction is correct

--- server/services/verifyCode.js
+++ server/services/verifyCode.js
@@ -8,12 +8,12 @@ module.exports = function(code) {
     console.log(code);
 
     // Get result from a sandboxed environment
-    var result = {bearTo: 270}
+    var result = {}
 
     // Check bearing
     console.log(result['bearTo']);
     try{
-        expect(result['bearTo']).to.equal(271);
+        expect([undefined,0,90,180,270]).to.include(result['bearTo']);
     } catch(e){
         console.log("Bad user!")
         console.log(e)
