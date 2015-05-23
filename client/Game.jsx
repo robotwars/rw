6e72ec31379e72d221599f52710a686a7a4879ad
Michael Pope <map7@corsairsolutions.com.au>
2015-05-23 16:39:48 +1000

    start adding react canvas stuff

--- client/Game.jsx
+++ client/Game.jsx
@@ -1,11 +1,19 @@
-import React   from 'react';
+import React       from 'react';
+import ReactCanvas from 'react-canvas';
 
 class Game extends React.Component {
 
   render() {
+    const surfaceWidth = window.innerWidth;
+    const surfaceHeight = window.innerHeight;
+    const textStyle = this.getTextStyle();
+
     return (
       <div>
-        GAME
+        Game
+      <surface width={surfaceWidth} height={surfaceHeight} left={0} top={0}>
+      <text style={textStyle}></text>
+      </surface>
       </div>
     );
   }
