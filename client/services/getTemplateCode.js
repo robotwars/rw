55c6ff99e6e1d8270eac571137b2b2efccb48983
Sebastian Porto <sebasporto@gmail.com>
2015-05-24 19:12:57 +1000

    Template

--- client/services/getTemplateCode.js
+++ client/services/getTemplateCode.js
@@ -3,13 +3,19 @@ module.exports = function() {
   return `// Your code here
 function(args) {
 
+  var status = args.status;
+  var walls = args.radar.walls;
+  var robots = args.radar.robots;
+  var logs = [];
+
+  logs.push('Show this in my console');
 
   return {
-    bearTo: undefined,
-    move:   0,
-    aimTo:  undefined,
+    bearTo:    undefined,
+    move:      0,
+    aimTo:     undefined,
     useWeapon: undefined,
-    logs: []
+    logs:      logs
   };
 }
 `;
