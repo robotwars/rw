5c677e78ef65c57fcfc2bc3b401db1e563f66a60
Michael Pope <map7@corsairsolutions.com.au>
2015-05-24 14:34:18 +1000

    Add some nice logs to a little guy

--- server/services/calcGameStateChanges.js
+++ server/services/calcGameStateChanges.js
@@ -8,7 +8,28 @@ And the responses from the robots
 And calculates the new state for the robots
 */
 
+
+
+
 module.exports = function(args) {
+
+  // Go get our robot
+  function findRobot(robot){
+    var robot = _.find(args.responses,function(r){
+      return r.robotId == robot.id;
+    })
+    return robot;
+  }
+
+  // Is there a robot at this position
+  function findRobotAtPos(x, y){
+    var robot = _.find(args.responses,function(r){
+      return r.robotId == robot.id;
+    })
+    return robot;
+  }
+  
+
   if (!args.responses)     throw new 'response is required';
   if (!args.prevGameState) throw new 'prevGameState is required';
   if (!args.prevGameState.robots) throw new 'prevGameState.robots is required';
@@ -20,12 +41,12 @@ module.exports = function(args) {
   _.each(robots, function(robot) {
     var x = robot.x || 0;
     var y = robot.y || 0;
-    
-    var response = _.find(args.responses,function(r){
-      return r.robotId == robot.id;
-    })
 
-    console.log("RESPONSE", response.move)
+    var response = findRobot(robot)
+    console.log("RESPONSE", response)
+
+    // Transfer the logs over to the robot object
+    robot.logs = response.logs;
     
     // look at the move and change x, y accordingly
     if(response.bearTo == 0){
@@ -35,7 +56,7 @@ module.exports = function(args) {
         robot.y += 1;
       }
     }else if(response.bearTo == 90){
-      if(response.move > 0){
+      if(response.move > 0 && robot.x != gameState.x){
         robot.x += 1;
       }else if(response.move < 0){
         robot.x -= 1;
@@ -55,10 +76,14 @@ module.exports = function(args) {
     }
 
     // colision detection and move back & take health off both (call this a RAM!)
+    if(robot.x){
 
-
+    }
 
     // weapons!!! Let's start with saw.
+    if(response.aimTo == 90){
+      console.log("Attack robot to the right with weapon: ", response.useWeapon)
+    }
 
     // detect which weapon is in use & which direction and if there is a robot in there
 
@@ -68,7 +93,7 @@ module.exports = function(args) {
     // Check boundaries
     if (robot.x > gameState.x) robot.x = gameState.x - 1;
     if (robot.x < 0) robot.x = 0;
-    if (robot.y > gameState.y) robot.y = gameState.y - 1;
+    if (robot.y > gameState.y) robot.y = gameState.y;
     if (robot.y < 0) robot.y = 0;
   });
 
